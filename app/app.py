d33f798f645ad6d9078916a43b57eb14652655a9
Keaton Nordby <keatonnordby4@gmail.com>
2026-09-21 21:00:05 -0500

    post enpoint added and response validation

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate, PostResponse
 
 
 app = FastAPI()
@@ -40,6 +41,7 @@ text_posts = {
 
 
 @app.get("/posts")
+
 def get_all_posts(limit: int = None):
     if limit:
         return list(text_posts.values())[:limit]
@@ -47,7 +49,16 @@ def get_all_posts(limit: int = None):
 
 
 @app.get("/posts{id}")
-def get_post(id: int):
+def get_post(id: int) -> PostResponse:
     if id not in text_posts:
         raise HTTPException(status_code=404, detail="Post not found")
     return text_posts.get(id)
+
+
+@app.post("/posts")
+def create_post(post: PostCreate) -> PostResponse:
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return new_post
+
+
